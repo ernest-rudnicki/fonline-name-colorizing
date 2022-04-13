a58efde2bd5bcb073e884a924cc4529bc14eebc5
Ernest Rudnicki <rudnicki.ernest@gmail.com>
2022-04-13 22:24:19 +0200

    chore(frontend): remove comment mistake

--- frontend/src/StoreWrappedApp.jsx
+++ frontend/src/StoreWrappedApp.jsx
@@ -3,7 +3,7 @@ import { Provider } from "react-redux";
 import App from "components/App";
 import { store } from "store/store";
 
-// React redux Provider is conflicting with the types provided by Preact, therefore I can only provide the store is via JavaScript
+// React redux Provider is conflicting with the types provided by Preact, therefore I can only provide the store via JavaScript
 const StoreWrappedApp = () => {
   return (
     <Provider store={store}>
